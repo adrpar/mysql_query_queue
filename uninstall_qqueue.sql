6696c1b39e4f7bf6ba8e1db9d3e59bd80754e675
Jochen Klar <jklar@aip.de>
2014-06-05 15:59:15 +0200

    IF EXISTS added

--- uninstall_qqueue.sql
+++ uninstall_qqueue.sql
@@ -1,18 +1,18 @@
 -- getting rid of all the data tables (you can skip this if you just want to reinstall)
-DROP TABLE mysql.qqueue_usrGrps;
-DROP TABLE mysql.qqueue_queues;
-DROP TABLE mysql.qqueue_jobs;
-DROP TABLE mysql.qqueue_history;
+DROP TABLE IF EXISTS mysql.qqueue_usrGrps;
+DROP TABLE IF EXISTS mysql.qqueue_queues;
+DROP TABLE IF EXISTS mysql.qqueue_jobs;
+DROP TABLE IF EXISTS mysql.qqueue_history;
 
 -- uninstalling all the UDF functions
-DROP FUNCTION qqueue_addUsrGrp;
-DROP FUNCTION qqueue_updateUsrGrp;
-DROP FUNCTION qqueue_flushUsrGrps;
-DROP FUNCTION qqueue_addQueue;
-DROP FUNCTION qqueue_updateQueue;
-DROP FUNCTION qqueue_flushQueues;
-DROP FUNCTION qqueue_addJob;
-DROP FUNCTION qqueue_killJob;
+DROP FUNCTION IF EXISTS qqueue_addUsrGrp;
+DROP FUNCTION IF EXISTS qqueue_updateUsrGrp;
+DROP FUNCTION IF EXISTS qqueue_flushUsrGrps;
+DROP FUNCTION IF EXISTS qqueue_addQueue;
+DROP FUNCTION IF EXISTS qqueue_updateQueue;
+DROP FUNCTION IF EXISTS qqueue_flushQueues;
+DROP FUNCTION IF EXISTS qqueue_addJob;
+DROP FUNCTION IF EXISTS qqueue_killJob;
 
 -- uninstalling all the procedures
 USE mysql;
@@ -20,5 +20,15 @@ DROP PROCEDURE IF EXISTS qqueue_clean_history;
 DROP PROCEDURE IF EXISTS qqueue_wipe_history;
 
 -- stop the daemon and uninstall
-UNINSTALL PLUGIN qqueue;
+USE mysql;
+DELIMITER //
+DROP PROCEDURE IF EXISTS UninstallPlugin;
+CREATE PROCEDURE UninstallPlugin()
+BEGIN
+IF (SELECT 1 = 1 FROM mysql.plugin WHERE `name`='qqueue') THEN UNINSTALL PLUGIN qqueue;
+END IF;
+END //
+DELIMITER ;
+CALL UninstallPlugin();
+DROP PROCEDURE UninstallPlugin;
 
